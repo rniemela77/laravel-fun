27e8f88cbbc40c78bd891b5c6dab21fafeca0b61
Robert Niemela <rvniemela@hotmail.com>
2022-06-23 13:25:24 -0700

    refactor: reduce card padding in single listing page

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,7 +6,7 @@
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <x-card class="p-24">
+        <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{ asset('images/pear-closeup.png') }}" alt="" />
 
