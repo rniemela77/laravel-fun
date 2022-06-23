49dcc75ade508cb871769878e2925a306bea86c7
Robert Niemela <rvniemela@hotmail.com>
2022-06-23 13:34:15 -0700

    fix: use correct pear img

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -8,7 +8,7 @@
     <div class="mx-4">
         <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
-                <img class="w-48 mr-6 mb-6" src="{{ asset('images/pear-closeup.png') }}" alt="" />
+                <img class="w-48 mr-6 mb-6" src="{{ asset('images/pear-closeup.jpg') }}" alt="" />
 
                 <h3 class="text-2xl mb-2">{{ $listing->title }}</h3>
                 <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
