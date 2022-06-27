8eee1c4c5238bc59d3d5991ccb69ec6d24422957
Robert Niemela <rvniemela@hotmail.com>
2022-06-26 19:39:08 -0700

    feat: add page to create listings
    feat: add form validation and set POST request for /create
    
    
    fix: put data in db

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,18 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     // Show all listings
-    public function index() {
+    public function index()
+    {
         return view('listings.index', [
             'listings' => Listing::latest()->filter(request(['tag', 'search']))->get(),
         ]);
     }
 
     // Show single listing
-    public function show(Listing $listing) {
-        return view('listings.show', [ 'listing' => $listing ]);
+    public function show(Listing $listing)
+    {
+        return view('listings.show', ['listing' => $listing]);
+    }
+
+    // Show Create Form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+
+        // Session::flash('message', 'Listing Created Successfully!');
+
+        return redirect('/')->with('message', 'Listing Created Successfully!');
     }
 }
